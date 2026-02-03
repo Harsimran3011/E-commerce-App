66eb0c15f2dd30bbf861f4ee388193581fa16ab9
Harsimran singh <singhharsimran5200@gmail.com>
2026-02-04 00:06:42 +0530

    JS Added for SideTab

--- Main/script.js
+++ Main/script.js
@@ -1,102 +1,105 @@
-function productsSection() {
+let products = [
+    {
+        name: "Wireless Earbuds, IPX8",
+        price: "$89.99",
+        image: "img/red.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "JBL Headphones, X81P",
+        price: "$50.99",
+        image: "img/jbl.jpg",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Green Exclusive, P56Q",
+        price: "$169.99",
+        image: "img/green.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Earphones, LK56",
+        price: "$40.99",
+        image: "img/small.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Zebronics Headphones",
+        price: "$35.99",
+        image: "img/game.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Wired Earphone, HOQ56",
+        price: "$22.99",
+        image: "img/wired.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Gaming Headphone, 56PQ",
+        price: "$19.99",
+        image: "img/ing.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Patron Headphone",
+        price: "$11.99",
+        image: "img/la.jpg",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Red JBL Headphone",
+        price: "$89.99",
+        image: "img/red.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "White-Purple Headphone",
+        price: "$50.99",
+        image: "img/jbl.jpg",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Dark JBL Headphone",
+        price: "$169.99",
+        image: "img/green.webp",
+        rating: 6,
+        reviews: 121
+    },
+    {
+        name: "Sky Blue Earbuds IPX8",
+        price: "$40.99",
+        image: "img/small.webp",
+        rating: 6,
+        reviews: 121
+    }
+];
+products = products.map((p, i) => ({ ...p, id: i }))
 
-    let products = [
-        {
-            name: "Wireless Earbuds, IPX8",
-            price: "$89.99",
-            image: "img/red.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "JBL Headphones, X81P",
-            price: "$50.99",
-            image: "img/jbl.jpg",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Green Exclusive, P56Q",
-            price: "$169.99",
-            image: "img/green.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Earphones, LK56",
-            price: "$40.99",
-            image: "img/small.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Zebronics Headphones",
-            price: "$35.99",
-            image: "img/game.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Wired Earphone, HOQ56",
-            price: "$22.99",
-            image: "img/wired.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Gaming Headphone, 56PQ",
-            price: "$19.99",
-            image: "img/ing.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Patron Headphone",
-            price: "$11.99",
-            image: "img/la.jpg",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Red JBL Headphone",
-            price: "$89.99",
-            image: "img/red.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "White-Purple Headphone",
-            price: "$50.99",
-            image: "img/jbl.jpg",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Dark JBL Headphone",
-            price: "$169.99",
-            image: "img/green.webp",
-            rating: 6,
-            reviews: 121
-        },
-        {
-            name: "Sky Blue Earbuds IPX8",
-            price: "$40.99",
-            image: "img/small.webp",
-            rating: 6,
-            reviews: 121
-        }
-    ];
+let cartBtns = []
+
+function productsSection() {
 
     let inp = document.querySelector(".input input");
     let grid = document.querySelector(".grid");
 
+
     function showProducts(prd) {
         grid.innerHTML = "";
-        prd.forEach(function (proo) {
+        prd.forEach(function (proo, idx) {
 
             let main = document.createElement("div");
             main.classList.add("one");
-
 
             let img = document.createElement("img");
             img.src = proo.image;
@@ -114,52 +117,74 @@ function productsSection() {
             btn.classList.add("add-btn");
             btn.textContent = "Add to Cart";
 
-            getBtn(btn);
+            btn.dataset.idx = idx
 
             main.appendChild(img);
             main.appendChild(h3);
             main.appendChild(span);
             main.appendChild(btn);
             grid.appendChild(main);
-            console.log(btn)
-        });
 
+            cartBtns.push(btn)
+        });
     };
 
-    
-    
     inp.addEventListener("input", function () {
         let newOn = products.filter(function (u) {
             return u.name.toLowerCase().startsWith(inp.value.toLowerCase());
-            
         });
-        
-        
+
         showProducts(newOn);
     });
-    
-    
+
     showProducts(products);
-    
 }
+
+productsSection(products)
+
+
 
 let sideTab = document.querySelector('.page1 .sideTab')
 let sideIcon = document.querySelector('.nav-bar #cart')
 let sideClose = document.querySelector('.sideItems .top .close')
-let cartBtn = document.querySelector('.page1 .products .add-btn')
 
-sideIcon.addEventListener("click",  () => {
-    sideTab.style.right = "0"
+sideIcon.addEventListener("click", () => { sideTab.style.right = "0" })
+sideClose.addEventListener("click", () => { sideTab.style.right = "-22%" })
+
+
+cartBtns.forEach(function (e) {
+    e.onclick = function () {
+        let idx = this.dataset.idx
+        let product = products[idx]
+
+        if (idx === undefined) {
+            console.error("data-idx missing on button");
+            return;
+        }
+
+        cartProduct = document.querySelector('.sideTab .sideItems .sideProducts')
+
+        productDiv = document.createElement('div')
+        productDiv.classList.add('products')
+
+        cartImg = document.createElement('img')
+        cartImg.src = product.image
+        cartImg.classList.add('cartImg')
+
+        imgDetails = document.createElement('div')
+        imgDetails.classList.add('img-details')
+        imgDetails.innerHTML = product.name
+
+        cartCrossIcon = document.querySelector('.sideProducts i')
+
+        productDiv.prepend(imgDetails)
+        productDiv.prepend(cartImg)
+
+        let productIcon = document.querySelector('.sideProducts .products i').style.display = "initial"
+
+        cartProduct.appendChild(productDiv)
+
+        console.log(idx)
+
+    }
 })
-
-sideClose.addEventListener("click",  () => {
-    sideTab.style.right = "-22%"
-})
-
-let btn = function getBtn(btn) {
-    return btn;
-}
-console.log (btn)
-
-
-productsSection();
